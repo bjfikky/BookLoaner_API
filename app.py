5a5dfe47b06fe525b85c521ba61ded70dc9f45e7
Benjamin Orimoloye <bjfikky@yahoo.com>
2018-10-07 19:29:53 -0400

    Added CORS to allow localhost connection

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 
 import models
 
-from  resources.students import students_api
-from  resources.books import books_api
+from resources.students import students_api
+from resources.books import books_api
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(students_api, url_prefix='/api/v1')
 app.register_blueprint(books_api, url_prefix='/api/v1')
 
